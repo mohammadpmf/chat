5e4bca1bd55547f515a8dbbeeed170584a2f62e0
mohammadpmf <mohamamad.pfallah@gmail.com>
2022-03-07 23:52:33 +0330

    second_version

--- chat1.py
+++ chat1.py
@@ -1,25 +1,40 @@
-import socket, threading
+import socket, threading, tkinter as tk, tkinter.messagebox as ms
 print ("Starting server: ")
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(('127.0.0.1', 55555))
+s.bind(('127.0.0.1', 55556))
 s.listen()
 conn, addr = s.accept()
 
-def f2():
-    message = input("Enter message: ")
+def f2(event):
+    # message = input("Enter message: ")
+    message = entry.get()
     message = bytes(message, 'utf-8')
+    entry.delete(0,tk.END)
     conn.sendall(message)
-    f2()
 
 def f1():
     try:
-        threading.Thread(target=f2).start()
-        print('Connected by', addr)
+        # threading.Thread(target=f2).start()
+        ms.showinfo('Successfull Connection', f'Connected by {addr}')
         while True:
             data = conn.recv(1024).decode()
-            print(f"{addr} sent: {data}")
+            t.set(f"{addr} sent: {data}")
     except:
-        print("Connection Lost!")
+        ms.showerror("Connection Error.", "Connection Lost!")
 
-
+####################################################### tkinter
+root = tk.Tk()
+root.geometry('600x600+10+200')
+entry = tk.Entry(root)
+entry.bind("<Return>", f2)
+t = tk.StringVar(root)
+t.set("Text will be shown here!")
+lbl = tk.Label(root, textvariable=t, wraplength=500)
+btn = tk.Button(root, text='Send', command=lambda:f2("Alaki String"))
+entry.place(relx=0.05, rely=0.05, width=500)
+lbl.place(relx=0.05, rely=0.1)
+btn.place(relx=0.45, rely=0.85)
+###############################################################
 threading.Thread(target=f1).start()
+tk.mainloop()
+
--- chat2.py
+++ chat2.py
@@ -1,21 +1,38 @@
-import socket, threading
+import socket, threading, tkinter as tk, tkinter.messagebox as ms
 print("Starting Client: ")
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('127.0.0.1', 55555))
+s.connect(('127.0.0.1', 55556))
 
 def f2():
-    data = s.recv(1024).decode()
-    pretty_point = str(s).find("raddr=") + 6 # 6 ta ham bekhater hamin horoofe raddr=
-    addr = str(s)[pretty_point:-1]
-    print(f"{addr} sent: {data}")
-    threading.Timer(1, f2).start()
-
-def f1():
-    threading.Thread(target=f2).start()
-    while True:
-        message = input("Enter message: ")
-        message = bytes(message, 'utf-8')
-        s.sendall(message)
+    try:
+        while True:
+            data = s.recv(1024).decode()
+            pretty_point = str(s).find("raddr=") + 6 # 6 ta ham bekhater hamin horoofe raddr=
+            addr = str(s)[pretty_point:-1]
+            t.set(f"{addr} sent: {data}")
+    except:
+        ms.showerror("Connection Error.", "Connection Lost!")
 
 
-threading.Thread(target=f1).start()
+def f1(event):
+    message = entry.get()
+    message = bytes(message, 'utf-8')
+    entry.delete(0,tk.END)
+    s.sendall(message)
+
+####################################################### tkinter
+root = tk.Tk()
+root.geometry('600x600+610+200')
+entry = tk.Entry(root)
+entry.bind("<Return>", f1)
+t = tk.StringVar(root)
+t.set("Text will be shown here!")
+lbl = tk.Label(root, textvariable=t, wraplength=500)
+btn = tk.Button(root, text='Send', command=lambda:f1("Alaki String"))
+entry.place(relx=0.05, rely=0.05, width=500)
+lbl.place(relx=0.05, rely=0.1)
+btn.place(relx=0.45, rely=0.85)
+###############################################################
+threading.Thread(target=f2).start()
+tk.mainloop()
+
