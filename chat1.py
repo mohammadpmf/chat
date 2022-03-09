c1ded393cee2ffec03b2ca348b165c8e25418029
mohammadpmf <mohamamad.pfallah@gmail.com>
2022-03-09 10:58:22 +0330

    chat_v3

--- chat1.py
+++ chat1.py
@@ -1,4 +1,8 @@
-import socket, threading, tkinter as tk, tkinter.messagebox as ms
+from email import message
+import socket, threading, tkinter as tk, tkinter.ttk as ttk, tkinter.messagebox as msb
+from datetime import datetime
+MY_WIDTH = 900
+
 print ("Starting server: ")
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('127.0.0.1', 55556))
@@ -11,30 +15,52 @@ def f2(event):
     message = bytes(message, 'utf-8')
     entry.delete(0,tk.END)
     conn.sendall(message)
+    now = datetime.now()
+    date_time = now.strftime("%m/%d/%Y, %H:%M:%S")
+    previous = t.get() + '\n'
+    t.set(f"{previous}{message.decode()}\t\t\t\t\tsent at {date_time}")
 
 def f1():
     try:
         # threading.Thread(target=f2).start()
-        ms.showinfo('Successfull Connection', f'Connected by {addr}')
+        msb.showinfo('Successfull Connection', f'Connected by {addr}')
+        lbl_frame.config(text=f"You are chatting with {addr}")
         while True:
+            now = datetime.now()
+            date_time = now.strftime("%m/%d/%Y, %H:%M:%S")
             data = conn.recv(1024).decode()
-            t.set(f"{addr} sent: {data}")
+            previous = t.get() + '\n'
+            t.set(f"{previous}Recived: {data}\t\t\t\t\tat {date_time}")
     except:
-        ms.showerror("Connection Error.", "Connection Lost!")
+        msb.showerror("Connection Error.", "Connection Lost!")
+
+def clear(event='Alaki event'):
+    t.set("")
+    
 
 ####################################################### tkinter
 root = tk.Tk()
-root.geometry('600x600+10+200')
+root.config(bg='sky blue')
+root.title("Chat1")
+root.geometry(f'{MY_WIDTH}x600+0+200')
+root.bind("<Delete>", clear)
+lbl_frame = ttk.Labelframe(root, text="Chat Box")
 entry = tk.Entry(root)
 entry.bind("<Return>", f2)
 t = tk.StringVar(root)
-t.set("Text will be shown here!")
-lbl = tk.Label(root, textvariable=t, wraplength=500)
-btn = tk.Button(root, text='Send', command=lambda:f2("Alaki String"))
-entry.place(relx=0.05, rely=0.05, width=500)
-lbl.place(relx=0.05, rely=0.1)
-btn.place(relx=0.45, rely=0.85)
+lbl = tk.Label(lbl_frame, textvariable=t, wraplength=MY_WIDTH*0.9)
+frame_btns = tk.Frame(root, bg='sky blue')
+btn = tk.Button(frame_btns, text='Send', command=lambda:f2("Alaki String"))
+btn_clear = tk.Button(frame_btns, text='Clear', command=clear)
+btn_exit = tk.Button(frame_btns, text='End Chat', command=root.destroy)
+
+entry.pack(side='top', fill='x', padx=8, pady=4)
+lbl_frame.pack(side='top', expand=True, fill='both', padx=8, pady=4)
+lbl.pack(expand=True, fill='both', padx=8, pady=4)
+frame_btns.pack(side='bottom', padx=8, pady=4)
+btn.pack(side='left', padx=40, pady=3, expand=1)
+btn_clear.pack(side='left', padx=40, pady=3)
+btn_exit.pack(side='right', padx=40, pady=3)
 ###############################################################
 threading.Thread(target=f1).start()
 tk.mainloop()
-
--- chat2.py
+++ chat2.py
@@ -1,17 +1,24 @@
-import socket, threading, tkinter as tk, tkinter.messagebox as ms
+import socket, threading, tkinter as tk, tkinter.ttk as ttk, tkinter.messagebox as msb
+from datetime import datetime
+MY_WIDTH = 900
+
 print("Starting Client: ")
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(('127.0.0.1', 55556))
+pretty_point = str(s).find("raddr=") + 6 # 6 ta ham bekhater hamin horoofe raddr=
+addr = str(s)[pretty_point:-1]
 
 def f2():
     try:
+        lbl_frame.config(text=f"You are chatting with {addr}")
         while True:
+            now = datetime.now()
+            date_time = now.strftime("%m/%d/%Y, %H:%M:%S")
             data = s.recv(1024).decode()
-            pretty_point = str(s).find("raddr=") + 6 # 6 ta ham bekhater hamin horoofe raddr=
-            addr = str(s)[pretty_point:-1]
-            t.set(f"{addr} sent: {data}")
+            previous = t.get() + '\n'
+            t.set(f"{previous}Recived: {data}\t\t\t\t\tat {date_time}")
     except:
-        ms.showerror("Connection Error.", "Connection Lost!")
+        msb.showerror("Connection Error.", "Connection Lost!")
 
 
 def f1(event):
@@ -19,19 +26,37 @@ def f1(event):
     message = bytes(message, 'utf-8')
     entry.delete(0,tk.END)
     s.sendall(message)
+    now = datetime.now()
+    date_time = now.strftime("%m/%d/%Y, %H:%M:%S")
+    previous = t.get() + '\n'
+    t.set(f"{previous}{message.decode()}\t\t\t\t\tsent at {date_time}")
+
+def clear(event='Alaki event'):
+    t.set("")
 
 ####################################################### tkinter
 root = tk.Tk()
-root.geometry('600x600+610+200')
+root.config(bg='sky blue')
+root.title("Chat2")
+root.geometry(f'{MY_WIDTH}x600+0+200')
+root.bind("<Delete>", clear)
+lbl_frame = ttk.Labelframe(root, text="Chat Box")
 entry = tk.Entry(root)
 entry.bind("<Return>", f1)
-t = tk.StringVar(root)
-t.set("Text will be shown here!")
-lbl = tk.Label(root, textvariable=t, wraplength=500)
-btn = tk.Button(root, text='Send', command=lambda:f1("Alaki String"))
-entry.place(relx=0.05, rely=0.05, width=500)
-lbl.place(relx=0.05, rely=0.1)
-btn.place(relx=0.45, rely=0.85)
+t = tk.StringVar(lbl_frame)
+lbl = tk.Label(lbl_frame, textvariable=t, wraplength=MY_WIDTH*0.9)
+frame_btns = tk.Frame(root, bg='sky blue')
+btn = tk.Button(frame_btns, text='Send', command=lambda:f1("Alaki String"))
+btn_clear = tk.Button(frame_btns, text='Clear', command=clear)
+btn_exit = tk.Button(frame_btns, text='End Chat', command=root.destroy)
+
+entry.pack(side='top', fill='x', padx=8, pady=4)
+lbl_frame.pack(side='top', expand=True, fill='both', padx=8, pady=4)
+lbl.pack(expand=True, fill='both', padx=8, pady=4)
+frame_btns.pack(side='bottom', padx=8, pady=4)
+btn.pack(side='left', padx=40, pady=3, expand=1)
+btn_clear.pack(side='left', padx=40, pady=3)
+btn_exit.pack(side='right', padx=40, pady=3)
 ###############################################################
 threading.Thread(target=f2).start()
 tk.mainloop()
