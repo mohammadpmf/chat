9673a9f1325809a578a18e762d66c5f1a2a4d60a
mohammadpmf <mohammad.pfallah@gmail.com>
2022-06-06 20:23:10 +0430

    so what

--- chat1.py
+++ chat1.py
@@ -1,6 +1,6 @@
-from email import message
 import socket, threading, tkinter as tk, tkinter.ttk as ttk, tkinter.messagebox as msb
 from datetime import datetime
+from tkinter import filedialog as fd
 MY_WIDTH = 900
 
 print ("Starting server: ")
@@ -8,6 +8,8 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('127.0.0.1', 55556))
 s.listen()
 conn, addr = s.accept()
+print(conn)
+print(addr)
 
 def f2(event):
     # message = input("Enter message: ")
@@ -37,8 +39,19 @@ def f1():
 def clear(event='Alaki event'):
     t.set("")
     
+def send_file():
+    def send_it(fname):
+        with open(fname, 'rb') as f:
+            data = f.read()
+            conn.sendall(data)
+        msb.showinfo("Sent!", f"{fname}'s Data Sent!\n But I'm not sure the receiver got it completely :D")
+    file_name = fd.askopenfilename()
+    if file_name in ['', ()]: # User didn't choose any file
+        return
+    threading.Thread(target=send_it, args=(file_name,)).start()
 
-####################################################### tkinter
+
+####################################### UI #######################################
 root = tk.Tk()
 root.config(bg='sky blue')
 root.title("Chat1")
@@ -50,6 +63,7 @@ entry.bind("<Return>", f2)
 t = tk.StringVar(root)
 lbl = tk.Label(lbl_frame, textvariable=t, wraplength=MY_WIDTH*0.9)
 frame_btns = tk.Frame(root, bg='sky blue')
+btn_send_file = tk.Button(frame_btns, text='Send File ...', command=send_file)
 btn = tk.Button(frame_btns, text='Send', command=lambda:f2("Alaki String"))
 btn_clear = tk.Button(frame_btns, text='Clear', command=clear)
 btn_exit = tk.Button(frame_btns, text='End Chat', command=root.destroy)
@@ -57,10 +71,11 @@ btn_exit = tk.Button(frame_btns, text='End Chat', command=root.destroy)
 entry.pack(side='top', fill='x', padx=8, pady=4)
 lbl_frame.pack(side='top', expand=True, fill='both', padx=8, pady=4)
 lbl.pack(expand=True, fill='both', padx=8, pady=4)
-frame_btns.pack(side='bottom', padx=8, pady=4)
-btn.pack(side='left', padx=40, pady=3, expand=1)
-btn_clear.pack(side='left', padx=40, pady=3)
+frame_btns.pack(side='bottom', padx=8, pady=4, fill='x')
+btn_send_file.pack(side='left', padx=40, expand=1)
 btn_exit.pack(side='right', padx=40, pady=3)
-###############################################################
+btn_clear.pack(side='right', padx=40, pady=3)
+btn.pack(side='right', padx=40, pady=3)
+####################################### End UI #######################################
 threading.Thread(target=f1).start()
 tk.mainloop()
--- chat2.py
+++ chat2.py
@@ -1,5 +1,6 @@
 import socket, threading, tkinter as tk, tkinter.ttk as ttk, tkinter.messagebox as msb
 from datetime import datetime
+from tkinter import filedialog as fd
 MY_WIDTH = 900
 
 print("Starting Client: ")
@@ -7,6 +8,7 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(('127.0.0.1', 55556))
 pretty_point = str(s).find("raddr=") + 6 # 6 ta ham bekhater hamin horoofe raddr=
 addr = str(s)[pretty_point:-1]
+
 
 def f2():
     try:
@@ -34,6 +36,19 @@ def f1(event):
 def clear(event='Alaki event'):
     t.set("")
 
+
+def send_file():
+    def send_it(fname):
+        with open(fname, 'rb') as f:
+            data = f.read()
+            s.sendall(data)
+        msb.showinfo("Sent!", f"{fname}'s Data Sent!\n But I'm not sure the receiver got it completely :D")
+    file_name = fd.askopenfilename()
+    if file_name in ['', ()]: # User didn't choose any file
+        return
+    threading.Thread(target=send_it, args=(file_name,)).start()
+
+
 ####################################################### tkinter
 root = tk.Tk()
 root.config(bg='sky blue')
@@ -46,6 +61,7 @@ entry.bind("<Return>", f1)
 t = tk.StringVar(lbl_frame)
 lbl = tk.Label(lbl_frame, textvariable=t, wraplength=MY_WIDTH*0.9)
 frame_btns = tk.Frame(root, bg='sky blue')
+btn_send_file = tk.Button(frame_btns, text='Send File ...', command=send_file)
 btn = tk.Button(frame_btns, text='Send', command=lambda:f1("Alaki String"))
 btn_clear = tk.Button(frame_btns, text='Clear', command=clear)
 btn_exit = tk.Button(frame_btns, text='End Chat', command=root.destroy)
@@ -53,10 +69,11 @@ btn_exit = tk.Button(frame_btns, text='End Chat', command=root.destroy)
 entry.pack(side='top', fill='x', padx=8, pady=4)
 lbl_frame.pack(side='top', expand=True, fill='both', padx=8, pady=4)
 lbl.pack(expand=True, fill='both', padx=8, pady=4)
-frame_btns.pack(side='bottom', padx=8, pady=4)
-btn.pack(side='left', padx=40, pady=3, expand=1)
-btn_clear.pack(side='left', padx=40, pady=3)
+frame_btns.pack(side='bottom', padx=8, pady=4, fill='x')
+btn_send_file.pack(side='left', padx=40, expand=1)
 btn_exit.pack(side='right', padx=40, pady=3)
+btn_clear.pack(side='right', padx=40, pady=3)
+btn.pack(side='right', padx=40, pady=3)
 ###############################################################
 threading.Thread(target=f2).start()
 tk.mainloop()
